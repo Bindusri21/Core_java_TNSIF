8992b60063d9f227b84962032e3adaf43f285fe2
bindu sri <bindusri@bindus-MacBook-Air.local>
2025-08-06 22:38:25 +0530

    Initial push of Core Java TNSIF project

---
+++ src/junior_developer/kia_airbags.java
@@ -0,0 +1,10 @@
+package junior_developer;
+
+public class kia_airbags {
+		
+		public void disp_airbags()
+		{
+			System.out.println("All Kia have airbags");
+		}
+
+	}
---
+++ src/junior_developer/kia_speedlimit.java
@@ -0,0 +1,10 @@
+package junior_developer;
+
+public class kia_speedlimit {
+
+	public void disp_speedlimit()
+	{
+		System.out.println("All KIA have speedlimit");
+	}
+
+}
